101a0e276d4a82426bb355da1a04d5935befc02d
Warissara48 <warissara_ar@cmu.ac.th>
2025-02-19 19:28:00 +0700

    Singleton Pattern

---
+++ AccessLog.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccessLog {
+    private static volatile AccessLog instance;
+    private List<String> logs = new ArrayList<>();
+
+    private AccessLog() {}
+
+    public static AccessLog getInstance() {
+        if (instance == null) {
+            synchronized (AccessLog.class) {
+                if (instance == null) {
+                    instance = new AccessLog();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void logEntry(String entry) {
+        logs.add(entry);
+        System.out.println(entry); // แสดงผล Log ใน Console
+    }
+
+    public List<String> getLogs() {
+        return new ArrayList<>(logs); // ส่งสำเนาเพื่อป้องกันการแก้ไขโดยตรง
+    }
+}
